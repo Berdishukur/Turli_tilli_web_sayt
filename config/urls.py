72c83884462cd28ef64f9a3fb23d0a9ce715c859
Berdishukur <boboqulovberdishukur@gmail.com>
2024-12-06 18:18:37 +0500

    add

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('',include('blog.urls')),
     # path('',include('my_app.urls'))
 ]
