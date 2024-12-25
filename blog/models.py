81542cbbfd256d1553f1ca8be0f071263d48b294
Berdishukur <boboqulovberdishukur@gmail.com>
2024-12-25 11:50:39 +0500

    add

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name=models.CharField(max_length=10,blank=True,null=True)
-    age=models.SmallIntegerField(max_length=2,blank=False,null=False)
+    age=models.SmallIntegerField(max_length=2,blank=True,null=True)
+    def __init__(self):
+        return self.name
 
