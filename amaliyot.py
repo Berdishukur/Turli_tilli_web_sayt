2d98b93807f790e11eaa1a38dc3c2ccd5a54777d
Berdishukur <boboqulovberdishukur@gmail.com>
2025-01-06 18:11:27 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,6 +1,21 @@
-ism="Ali"
-familiya="Valiyev"
-yosh=23
+# x=int(input(" X= "))
+# y=3*x**6-6*x**2-7
+# print("Nateja = ",y)
+
+
+
+# a,b=12,55
+# a,b=b,a
+# print("A= ",a)
+# print("B= ",b)
+
+# Ali=Vali=17
+# print(Ali)
+# print(Vali)
+
+# ism="Ali"
+# familiya="Valiyev"
+# yosh=23
 
 # ism1="Ali"
 # print(ism1)
