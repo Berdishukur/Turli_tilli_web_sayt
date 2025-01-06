03ae3c067d122354fde483cb297bca0bdb208db5
Berdishukur <boboqulovberdishukur@gmail.com>
2025-01-06 16:25:00 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,7 +1,9 @@
-# print("    *")
-# print("  * *")
-# print(" *  *")
-# print("    *")
-# print("    *")
-# print("    *")
-# print("    *")
+ism="Ali"
+familiya="Valiyev"
+yosh=23
+
+# ism1="Ali"
+# print(ism1)
+#
+# print=55
+# print(print)
