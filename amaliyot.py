c0f74f4a5a078cc63ffd174c2826be4ee9501a49
Berdishukur <boboqulovberdishukur@gmail.com>
2025-01-08 17:21:54 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,24 +1,5 @@
-# x=int(input(" X= "))
-# y=3*x**6-6*x**2-7
-# print("Nateja = ",y)
 
-
-
-# a,b=12,55
-# a,b=b,a
-# print("A= ",a)
-# print("B= ",b)
-
-# Ali=Vali=17
-# print(Ali)
-# print(Vali)
-
-# ism="Ali"
-# familiya="Valiyev"
-# yosh=23
-
-# ism1="Ali"
-# print(ism1)
-#
-# print=55
-# print(print)
+t_yily=int(input("Tug'ilgan yilingizni kiriting : "))
+joriy_yil=2025
+yosh=joriy_yil-t_yily
+print("Sizning yoshingiz = ",yosh)
