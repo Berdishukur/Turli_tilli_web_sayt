c0b02510697aa3bda9965cf3e3feb05d414e5456
Berdishukur <boboqulovberdishukur@gmail.com>
2025-01-25 13:27:15 +0500

    add

---
+++ amaliyot.py
@@ -0,0 +1,11 @@
+son=int(input("Ixtiyoriy sonni kiriting : "))
+print(son%2)
+if son%2==0:
+    print("Juft")
+
+# if son>0:
+#     print(son+2)
+# elif son<0:
+#     print("Manfiy")
+# else:
+#     print("Nol")
