4d3e3e328b5a3e421d18bcc5104dc1c842ea15ba
Berdishukur <boboqulovberdishukur@gmail.com>
2025-01-27 18:49:39 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,11 +1,2 @@
-son=int(input("Ixtiyoriy sonni kiriting : "))
-print(son%2)
-if son%2==0:
-    print("Juft")
-
-# if son>0:
-#     print(son+2)
-# elif son<0:
-#     print("Manfiy")
-# else:
-#     print("Nol")
+matn="Men kelajakda dasturchi bo'laman"
+print("dasturlar" not in matn)
