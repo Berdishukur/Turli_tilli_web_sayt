ba0b2ecc9279c6829b66b9b45981404286d121ad
Berdishukur <boboqulovberdishukur@gmail.com>
2025-01-29 19:24:20 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,2 +1,16 @@
-matn="Men kelajakda dasturchi bo'laman"
-print("dasturlar" not in matn)
+P=False
+Q=False
+R=18
+if A%2==1 and :
+    print("Shart bajarildi.")
+else:
+    print("Shart bajarilmadi.")
+
+
+
+# a=int(input("a ni kiriting : "))
+# b=int(input("b ni kiriting : "))
+# if a>10 and (b<5 or b>15):
+#     print("Shart bajarildi;")
+# else:
+#     print("Shart bajarilmadi:")
