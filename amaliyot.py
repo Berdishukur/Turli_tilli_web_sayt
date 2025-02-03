fffc99db6b99a2147665e1318e28f48429e7b287
Berdishukur <boboqulovberdishukur@gmail.com>
2025-02-03 19:18:49 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,7 +1,18 @@
-for i in range(1,10):
-    if i==7:
-        continue
+mevalar=["oLma","qovun","anor","olcha","banan","nok"]
+for i in mevalar:
     print(i)
+
+
+
+# son=1
+# for i in range(1,10):
+#     # 1,2,3,4,5,6,7,8,9
+#     son=son*i
+# print(son)
+# for i in range(1,10):
+#     if i==3 or i==7:
+#         continue
+#     print(i)
 
 
 
