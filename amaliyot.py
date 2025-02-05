6131838b314e2a2530f34fe964d56456d47d42d6
Berdishukur <boboqulovberdishukur@gmail.com>
2025-02-05 18:46:22 +0500

    add

---
+++ amaliyot.py
@@ -0,0 +1,28 @@
+n=int(input(" N= "))
+sum=0
+for i in range(1,n):
+    if i%2==1:
+        sum+=i
+print(sum)
+# K=5
+# N=7
+# for i in range(0,N):
+#     print(K)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
