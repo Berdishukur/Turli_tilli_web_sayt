7958da1848dade556c2a3bbd2f20259b83ae7ad5
Berdishukur <boboqulovberdishukur@gmail.com>
2025-02-06 17:49:58 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,28 +1,44 @@
-n=int(input(" N= "))
-sum=0
-for i in range(1,n):
-    if i%2==1:
-        sum+=i
-print(sum)
-# K=5
-# N=7
-# for i in range(0,N):
-#     print(K)
+# a=int(input())
+
+
+# a,b=5,15
+# a,b=b,a
+# print(a,b)
+
+# a="7"
+# print(type(a))
+# a=float(a)
+# print(type(a),a)
 
 
 
 
+# ism='Ali'
+# print(type(ism))
+# yosh=25
+# print(type(yosh))
+# narx=12.5000
+# print(type(narx))
+# a=24
+# print(a)
+# del a
 
 
 
+# Saidumar_buyi=Berdishukur_buyi=189
+# print(Saidumar_buyi)
+# print(Berdishukur_buyi)
 
 
+# ism,familiya,yosh,vazin,jinsi="Ali","Valiyev",24,"erkak",78
+# print(jinsi)
+# ism=input("Ismingizni kiriting : ")
+# print(ism)
+# ism2=input("Do'stingizni ismini kiriting : ")
+# print(ism2)
+# fullName=input("Enter full name ... ")
+# print(fullName)
 
-
-
-
-
-
-
-
-
+# a=4
+# a=6
+# print(a)
