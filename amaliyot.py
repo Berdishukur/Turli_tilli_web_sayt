c894c86210ae2d600d9cb33f6bb5efc349deb37a
Berdishukur <boboqulovberdishukur@gmail.com>
2025-02-07 17:10:39 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,7 +1,10 @@
-# N=int(input("N ni kiriting : "))
-# sum=0
-# while N>=0:
-#     print(N)
-#     sum+=N%10
-#     N=N//10
-# print(sum)
+N=int(input("N ni kiriting : "))
+sum=0
+while N>=0:
+    print(N)
+    sum+=N%10
+    N=N//10
+print(sum)
+
+
+
