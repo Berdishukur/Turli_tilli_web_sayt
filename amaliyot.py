6d16e6eaf3ffa281cd61ea6833aa76b9a9c2b7ca
Berdishukur <boboqulovberdishukur@gmail.com>
2025-02-13 17:35:36 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,12 +1,2 @@
-for i in range(0,5):
-    for j in range(0,5):
-        if i==0 or j==0 :
-            print("* ",end="")
-        else:
-            print("  ",end="")
-    print()
-
-
-
-# for i in "Asdfsdfdgfds":
-#     print(i.lower())
+matn="Dasturlash bu yaxshi soha"
+print()
