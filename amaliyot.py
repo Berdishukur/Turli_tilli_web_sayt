2c7f4f7195ea9bc0ad4c859f72b67167461dfc57
Berdishukur <boboqulovberdishukur@gmail.com>
2025-02-17 18:33:39 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,18 +1,9 @@
 
-def age_calulator(birthday):
-     print("Your age is ",2025-birthday)
-birthday=int(input("Enter birthday : "))
-age_calulator(birthday)
-# def malumot_ber(ism,yosh):
-#      print(f"Ism : {ism}\nYosh : {yosh}")
-# malumot_ber("Ali",25)
-# malumot_ber(25,"Vali")
-# def salom_ber(ism):
-#      """
-#      Salom beradigan funksiya yaratdim
-#      """
-#      print("Hello",ism)
-#
-# salom_ber("Ali")
-# salom_ber("Vali")
-# salom_ber("Bahodir")
+kiyimlar=["Mayka","Shim","ko'ylak","kurutka","Paypoq","Kepka"]
+new_clothes=[]
+i=0
+while i<len(kiyimlar):
+    if kiyimlar[i].lower().startswith("k"):
+        new_clothes.append(kiyimlar[i])
+    i+=1
+print(new_clothes)
