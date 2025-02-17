ef713d57e56402e09532f99f29391b0758fb1e41
Berdishukur <boboqulovberdishukur@gmail.com>
2025-02-17 17:23:16 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,20 +1,18 @@
-g=['*','*','*','*','*','*','*','*','*']
-print(f"""
-| {g[0]} | {g[1]} | {g[2]} |
-| {g[3]} | {g[4]} | {g[5]} |
-| {g[6]} | {g[7]} | {g[8]} |
-""")
-while True:
-    x=int(input(" X = "))
-    while g[x-1]=="X" or g[x-1]=="Y":
-        print("Bu joy band.")
-        x = int(input(" X = "))
-    g[x-1]="X"
-    print(f"""
-    | {g[0]} | {g[1]} | {g[2]} |
-    | {g[3]} | {g[4]} | {g[5]} |
-    | {g[6]} | {g[7]} | {g[8]} |
-    """)
-    if g[0]==g[1]==g[2]=="X" or g[0]==g[3]==g[6]=="X":
-        print("X yutdi, tamom!!!")
-        break
+
+def age_calulator(birthday):
+     print("Your age is ",2025-birthday)
+birthday=int(input("Enter birthday : "))
+age_calulator(birthday)
+# def malumot_ber(ism,yosh):
+#      print(f"Ism : {ism}\nYosh : {yosh}")
+# malumot_ber("Ali",25)
+# malumot_ber(25,"Vali")
+# def salom_ber(ism):
+#      """
+#      Salom beradigan funksiya yaratdim
+#      """
+#      print("Hello",ism)
+#
+# salom_ber("Ali")
+# salom_ber("Vali")
+# salom_ber("Bahodir")
