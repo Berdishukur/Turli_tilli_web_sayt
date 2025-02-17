35b1d52345b0101c65ef966ce1e91e6727bf16d7
Berdishukur <boboqulovberdishukur@gmail.com>
2025-02-17 19:27:54 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,9 +1,1 @@
 
-kiyimlar=["Mayka","Shim","ko'ylak","kurutka","Paypoq","Kepka"]
-new_clothes=[]
-i=0
-while i<len(kiyimlar):
-    if kiyimlar[i].lower().startswith("k"):
-        new_clothes.append(kiyimlar[i])
-    i+=1
-print(new_clothes)
