510f3c7bd6268180c7e49f37f3ab439452426cf2
Berdishukur <boboqulovberdishukur@gmail.com>
2025-02-26 17:57:38 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,5 +1,32 @@
-numbers1=[i for i in range(1,11) if i%2==1]
+numbers1=[num for num in range(1,31) if num%3==0 or num%4==0]
 print(numbers1)
+
+# result=lambda x,y:x+y
+# print(result(4,6))
+# for i in range(1,10):
+#     for j in range(1,10):
+#         print([i,j])
+# numbers=[[i,j] for i in range(1,10) for j in range(1,10) if i==j*2]
+# print(numbers)
+# def add(x,y):
+#     return x+y
+# nateja=add(2,4)
+# print(nateja)
+
+
+
+
+# words=["level", "world", "radar", "python"]
+# newList=[]
+# for word in words:
+#     if word==word[::-1]:
+#         newList.append(word)
+# print(newList)
+
+
+
+# numbers1=[i for i in range(1,11) if i%2==1]
+# print(numbers1)
 
 # fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 # newList=[fruit.upper() for fruit in fruits if "a" in fruit]
