328b6777e2af79f7343e80bf1d7ebf425d637c02
Berdishukur <boboqulovberdishukur@gmail.com>
2025-02-28 18:56:38 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,15 +1,24 @@
-   # Filter
-   # Reduce
-   # Map
-from functools import reduce
-sonlar=[1,2,3,4,5,6,7,8,9]
-result=reduce(lambda a,b:a if a>b else b,sonlar)
-print((result))
-# def even(n):
-#     return (n>0)
-# numbers1=[-1,3,12,-34,56]
-# result=list(filter(even,numbers1))
-# print(result)
+sonlar =[1,2,3,4,5,6,7,8]
+newlist=[i for i in sonlar if i>5]
+print(newlist)
+# son=25
+# print(type(son))
+# son=str(son)
+# print(type(son))
+
+
+#    # Filter
+#    # Reduce
+#    # Map
+# from functools import reduce
+# sonlar=[1,2,3,4,5,6,7,8,9]
+# result=reduce(lambda a,b:a if a>b else b,sonlar)
+# print((result))
+# # def even(n):
+# #     return (n>0)
+# # numbers1=[-1,3,12,-34,56]
+# # result=list(filter(even,numbers1))
+# # print(result)
 
 
 
