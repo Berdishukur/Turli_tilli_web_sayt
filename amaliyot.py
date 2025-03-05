49386246925a03f84a58d89ebf9c28f03490ce24
Berdishukur <boboqulovberdishukur@gmail.com>
2025-03-05 17:43:15 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,7 +1,7 @@
-# try:
-#     print(ism)
-# except NameError as i:
-#     print(f"Sizda >>> {i} <<< xatolik yuz berdi ")
-#
-# finally:
-#     print("Dastur yakunllandi")
+try:
+    print(ism)
+except NameError as i:
+    print(f"Sizda >>> {i} <<< xatolik yuz berdi ")
+
+finally:
+    print("Dastur yakunllandi")
