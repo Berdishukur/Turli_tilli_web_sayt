dc8e7ff44125f2adbaf0802f689c467788eef3c5
Berdishukur <boboqulovberdishukur@gmail.com>
2025-03-07 17:46:45 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,4 +1,14 @@
- # map
-numbers=[1,2,3,4,5,6]
-newlist=map(lambda x:x*7,numbers)
-print(list(newlist))
+
+class Person:
+    def __init__(self,age, name,gender,weight):
+        self.age=age
+        self.name=name
+        self.gender=gender
+        self.wieght=weight
+    def speak(self,word):
+        print(f"{self.name} odam gapirdi >>> {word} <<<")
+person1=Person(25,"Berdishukur",True,83)
+person2=Person(14,"Bahodir",True,60)
+person1.speak("Salom")
+# print(person1.name)
+# print(person2.name)
