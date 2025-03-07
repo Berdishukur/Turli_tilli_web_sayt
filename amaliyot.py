c2a787185d2ba3184f45bb98b342667067666350
Berdishukur <boboqulovberdishukur@gmail.com>
2025-03-07 18:16:43 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,14 +1,7 @@
-
-class Person:
-    def __init__(self,age, name,gender,weight):
-        self.age=age
-        self.name=name
-        self.gender=gender
-        self.wieght=weight
-    def speak(self,word):
-        print(f"{self.name} odam gapirdi >>> {word} <<<")
-person1=Person(25,"Berdishukur",True,83)
-person2=Person(14,"Bahodir",True,60)
-person1.speak("Salom")
-# print(person1.name)
-# print(person2.name)
+class Oson1:
+    def __init__(self,a):
+        self.a=a
+    def output_a(self):
+        print(self.a)
+oson1=Oson1(23)
+oson1.output_a()
