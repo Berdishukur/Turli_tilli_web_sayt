4f33a794c4afd1e22235d66fb8fc3328eb462628
Berdishukur <boboqulovberdishukur@gmail.com>
2025-03-26 19:30:22 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,12 +1,7 @@
-
-while True:
-    xotira=30
-    with open('my1.txt', 'a') as file:
-        file.write("\n" + input("Ma'lumotlarni kiriting : "))
-    with open('my1.txt', 'r') as file2:
-        s1 = file2.read()
-        uzunlik=len(s1)
-        print(uzunlik)
-    if uzunlik>xotira:
-        print("sizda joy qolmadi ")
-        break
+son = int(input('Son : '))
+if son>0:
+    print("Musbat")
+elif son==0:
+    print("Nol")
+else:
+    print("Manfiy")
