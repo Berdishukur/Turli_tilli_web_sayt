2d631ab1a0ebcc89bde55aa4cd68703e97ecd8d4
Berdishukur <boboqulovberdishukur@gmail.com>
2025-03-26 17:48:24 +0500

    add

--- amaliyot.py
+++ amaliyot.py
@@ -1,7 +1,12 @@
-# file=open('my1.txt','r')
-# s1=file.read()
-# uzunlik=len(s1)
-# file.close()
-with open('my1.txt', 'a') as file:
-    s1=file.read()
-    print(s1)
+
+while True:
+    xotira=30
+    with open('my1.txt', 'a') as file:
+        file.write("\n" + input("Ma'lumotlarni kiriting : "))
+    with open('my1.txt', 'r') as file2:
+        s1 = file2.read()
+        uzunlik=len(s1)
+        print(uzunlik)
+    if uzunlik>xotira:
+        print("sizda joy qolmadi ")
+        break
